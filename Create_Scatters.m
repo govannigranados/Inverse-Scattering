3f59efbf85d3c51dbff118d5305d645a45869c91
govannigranados <govanni.632@gmail.com>
2023-05-12 13:33:26 -0700

    Made 'centers' an input.

---
+++ Create_Scatters.m
@@ -0,0 +1,6 @@
+centers1 = [0 0.5; 0 -0.75; -0.25 -0.25; 0.25 0.25];
+Scatter1 = Scattering_Object(centers1);
+Scatter1.Visualize()
+centers2 = [0 0; 0 -0.75; -0.25 -0.25; 0.25 0.25];
+Scatter2 = Scattering_Object(centers2);
+Scatter2.Visualize()
